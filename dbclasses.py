231228f8c75ce7ed93dc698ab16841955e3dafba
Andrea Dotti <adotti@slac.stanford.edu>
2018-06-13 15:19:01 -0700

    Improve documentation
    Specify what is the issue with groups in this current design

--- dbclasses.py
+++ dbclasses.py
@@ -37,6 +37,20 @@ class Component(Base):
     #you define sub-classes
     __mapper_args__ = { 'polymorphic_on': component_type }
     # Allow for groups of components
+    # This is the part that I do not understand well, I copied online :-)
+    # Basically: I am creating a one-to-multi relationship, allowing
+    # A component to own sub-components. 
+    # The backref here after should be a way to keep the navigation
+    # in both ways. 
+    # Issue: I think in this desing I cannot have a component belonging to
+    # two separate groups
+    # This is because the relationship parent->daughter is expressed in the 
+    # daughter adding a parent. To understand what has to be changed here
+    # to allow to specify daughters and forget about parent.
+    # see function in testing module called *add_groups*
+    # The collecion_class stuff tells to use a dictionary to keep childrens
+    # using the property 'name' as key.
+    # See: http://docs.sqlalchemy.org/en/latest/_modules/examples/adjacency_list/adjacency_list.html
     parent_id = Column(Integer, ForeignKey(id))
     children = relationship(
         "Component",
